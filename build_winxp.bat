bf737318eaf94ebc3e80d279d7f4c7fe6b851a5e
horsicq <horsicq@gmail.com>
2020-04-30 11:01:01 +0200

    Fix

--- build_winxp.bat
+++ build_winxp.bat
@@ -1,6 +1,6 @@
-set VS_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community"
-set QT_PATH=C:\Qt\5.12.8\msvc2017
+set VS_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0"
 set SEVENZIP_PATH="C:\Program Files\7-Zip"
+set QT_PATH="C:\Qt\5.6.3\msvc2013"
 
 set BUILD_NAME=nfd_win32_portable
 set SOURCE_PATH=%~dp0
@@ -10,14 +10,15 @@ mkdir %SOURCE_PATH%\release
 set /p RELEASE_VERSION=<%SOURCE_PATH%\release_version.txt
 
 set QT_PATH=%QT_PATH%
-call %VS_PATH%\VC\Auxiliary\Build\vcvars32.bat
+set QT_SPEC=win32-msvc2013
+call %VS_PATH%\VC\bin\vcvars32.bat
 set GUIEXE=nfd.exe
 set CONEXE=nfdc.exe
 set ZIP_NAME=%BUILD_NAME%_%RELEASE_VERSION%
 set RES_FILE=rsrc
 
 cd gui_source
-%QT_PATH%\bin\qmake.exe gui_source.pro -r -spec win32-msvc "CONFIG+=release"
+%QT_PATH%\bin\qmake.exe gui_source.pro -r -spec %QT_SPEC% "CONFIG+=release"
 
 nmake Makefile.Release clean
 nmake
@@ -28,7 +29,7 @@ del Makefile.Debug
 cd ..
 
 cd console_source
-%QT_PATH%\bin\qmake.exe console_source.pro -r -spec win32-msvc "CONFIG+=release"
+%QT_PATH%\bin\qmake.exe console_source.pro -r -spec %QT_SPEC% "CONFIG+=release"
 
 nmake Makefile.Release clean
 nmake
@@ -60,8 +61,8 @@ copy %QT_PATH%\bin\Qt5Gui.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
 copy %QT_PATH%\bin\Qt5Core.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
 copy %QT_PATH%\plugins\platforms\qwindows.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\platforms\
 
-copy %VS_PATH%\VC\Redist\MSVC\14.16.27012\x86\Microsoft.VC141.CRT\msvcp140.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
-copy %VS_PATH%\VC\Redist\MSVC\14.16.27012\x86\Microsoft.VC141.CRT\vcruntime140.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
+copy %VS_PATH%\VC\redist\x86\Microsoft.VC120.CRT\msvcp120.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
+copy %VS_PATH%\VC\redist\x86\Microsoft.VC120.CRT\msvcr120.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
 
 cd %SOURCE_PATH%\release
 if exist %ZIP_NAME%.zip del %ZIP_NAME%.zip
--- gui_source/dialogabout.cpp
+++ gui_source/dialogabout.cpp
@@ -27,15 +27,8 @@ DialogAbout::DialogAbout(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    ui->labelInfo->setText(QString("<html><head/><body><p align=\"center\">"
-                                   "<span style=\" font-weight:600;\">Copyright(C) 2017-2019 hors</span></p>"
-                                   "<p align=\"center\"><span style=\" font-weight:600;\">"
-                                   "Version: </span>%1 (%2)</p><p align=\"center\">"
-                                   "<span style=\" font-weight:600;\">Website: </span><a href=\"http://ntinfo.biz\">"
-                                   "<span style=\" text-decoration: underline; color:#0000ff;\">http://ntinfo.biz</span>"
-                                   "</a></p><p align=\"center\"><span style=\" font-weight:600;\">Bugerports: </span>"
-                                   "<a href=\"mailto:horsicq@gmail.com\"><span style=\" text-decoration: underline; color:#0000ff;\">"
-                                   "horsicq@gmail.com</span></a></p></body></html>").arg(X_APPLICATIONVERSION).arg(__DATE__));
+    ui->labelVersion->setText(QString("<html><head/><body><p align=\"center\"><span style=\" "
+                                      "font-weight:600;\">Version:</span> %1(%2)</p></body></html>").arg(X_APPLICATIONVERSION).arg(__DATE__));
 }
 
 DialogAbout::~DialogAbout()
