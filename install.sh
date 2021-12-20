9bf5f4e41dbd41450621f6e99424d276f1d93839
horsicq <horsicq@gmail.com>
2021-12-20 08:36:19 +0100

    Fix: 2021-12-20

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ export X_SOURCE_PATH=$PWD
 
 cp -f $X_SOURCE_PATH/build/release/nfd                            /usr/bin/
 cp -f $X_SOURCE_PATH/build/release/nfdc                            /usr/bin/
-cp -f $X_SOURCE_PATH/DEBIAN/nfd.desktop                           /usr/share/applications/
-cp -Rf $X_SOURCE_PATH/DEBIAN/hicolor/                               /usr/share/icons/
+cp -f $X_SOURCE_PATH/LINUX/nfd.desktop                           /usr/share/applications/
+cp -Rf $X_SOURCE_PATH/LINUX/hicolor/                               /usr/share/icons/
