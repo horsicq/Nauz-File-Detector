11a5fbf85153aa3f963585c8b1e80767a5afd112
horsicq <horsicq@gmail.com>
2022-05-04 23:12:29 +0200

    Fix: 2022-05-04

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash -x
 export X_SOURCE_PATH=$PWD
 
-cp -f $X_SOURCE_PATH/build/release/nfd                            /usr/bin/
-cp -f $X_SOURCE_PATH/build/release/nfdc                            /usr/bin/
-cp -f $X_SOURCE_PATH/LINUX/nfd.desktop                           /usr/share/applications/
-cp -Rf $X_SOURCE_PATH/LINUX/hicolor/                               /usr/share/icons/
+if [ -z "$1" ]; then
+    X_PREFIX="/usr"
+else
+    X_PREFIX="$1"
+fi
+
+mkdir -p  $X_PREFIX/lib/bin
+mkdir -p  $X_PREFIX/lib/share/applications
+mkdir -p  $X_PREFIX/lib/share/icons
+
+cp -f $X_SOURCE_PATH/build/release/nfd                              $X_PREFIX/bin/
+cp -f $X_SOURCE_PATH/build/release/nfdc                             $X_PREFIX/bin/
+cp -f $X_SOURCE_PATH/LINUX/nfd.desktop                              $X_PREFIX/share/applications/
+cp -Rf $X_SOURCE_PATH/LINUX/hicolor/                                $X_PREFIX/share/icons/
