9205376c5480020e207f750c0fa737dbc9d42b50
horsicq <horsicq@gmail.com>
2023-06-21 23:54:09 +0200

    Fix: 2023-06-21

--- build_msvc_winxp.bat
+++ build_msvc_winxp.bat
@@ -1,4 +1,4 @@
-set VSVARS_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat"
+set VSVARS_PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\bin\vcvars32.bat"
 set QMAKE_PATH="C:\Qt5.6.3\5.6.3\msvc2013\bin\qmake.exe"
 set SEVENZIP_PATH="C:\Program Files\7-Zip\7z.exe"
 
