d8c38f1642a6eed716b1bbbbfa269a146cadc8f8
horsicq <horsicq@gmail.com>
2025-03-11 17:52:17 +0100

    Add new module

--- build_archpkg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-mkdir release
-cp -f PKGBUILD release/
-cd release
-makepkg -Acs
-rm -Rf PKGBUILD
-rm -Rf Nauz-File-Detector
-cd ..
--- build_libs/build_libs.pro
+++
@@ -1,3 +0,0 @@
-TEMPLATE      = subdirs
-SUBDIRS       += ../XArchive
-SUBDIRS       += ../XCapstone
