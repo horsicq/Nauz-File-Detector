6959de5f08761f41534c43f3d488493b4b4eb7b2
horsicq <horsicq@gmail.com>
2026-04-23 14:14:20 +0200

    chore: remove outdated build scripts

--- build/build_linux.sh
+++
@@ -1,9 +0,0 @@
-cd ..
-mkdir tmp_build
-cd tmp_build
-cmake -DCMAKE_BUILD_TYPE=MinSizeRel ..
-make
-cpack
-cd ..
-cp -Rf tmp_build/packages .
-
--- build/build_macos.sh
+++
@@ -1,9 +0,0 @@
-# sudo ~/Qt/Tools/CMake/CMake.app/Contents/bin/cmake-gui --install
-cd ..
-mkdir tmp_build
-cd tmp_build
-cmake -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_PREFIX_PATH="~/Qt/5.15.2/clang_64" ./..
-make
-cpack
-cd ..
-cp -Rf tmp_build/packages .
